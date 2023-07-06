179bb66b0df55a312f76ee8e974e5b0c3d92b0eb
Debashis Roy <goodquote39@hotmail.com>
2023-07-06 23:48:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {} from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Search Country for details</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
